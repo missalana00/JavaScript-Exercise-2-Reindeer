a16880bf2c7d446bc055f10e1980660d62905cd5
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2016-11-28 15:22:07 -0600

    Created Javascript exercise 2; completed first portion of task.

---
+++ reindeer.js
@@ -0,0 +1,18 @@
+
+
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var hohohoElement = document.getElementById("coloredReindeer"); 
+
+
+
+// 1. for loop - console logging
+// 2. outside of for loop - pg 176 mm
+	
+
+
+for (var i=0; i<reindeer.length; i++) {
+	console.log(reindeer[i] + " is " +  colors[i])
+}
